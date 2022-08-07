2c5c403e6458c9013f05f184435c699e28a6a484
Joao Treto <joaoatreto@gmail.com>
2022-08-07 20:37:25 -0300

    Featu

---
+++ ui.frontend/src/components/Micro/MyText/MyText.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p :style="{ 'color' : msgColor}">{{ msgHeader }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "MyText",
+  props: {
+    msgHeader: {
+        type: String,
+        default: 'Forms'
+    },
+    msgColor: {
+        type: String,
+        default: '#000000'
+    }
+  },
+};
+</script>
+
+<style lang="scss" scooped>
+p {
+  font-family: "Nunito", sans-serif;
+  font-weight: 400;
+  font-size: 1.2em;
+  width: 100%;
+  @media (max-width: 768px) {
+    font-size: 1.1em;
+  }
+}
+</style>
