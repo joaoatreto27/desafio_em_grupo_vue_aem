52713bb7574f6419495fcb24fb9b157ef24d2fa8
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-08-07 21:20:42 -0300

    Feat: Adding button component

---
+++ ui.frontend/src/components/Micro/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="containerButton">
+    <button :class="classButton" @click.prevent="event">{{ text }}</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    classButton: {
+      type: String
+    }
+  },
+  methods: {
+    event () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './Button.scss';
+</style>
