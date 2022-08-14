c6b1b669c7f86022db9f836daeaa6e6d348aaca0
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-08-14 18:02:59 -0300

    Fix: changing v-show after v-if

--- ui.frontend/src/components/Container/MySuccess/MySuccess.vue
+++ ui.frontend/src/components/Container/MySuccess/MySuccess.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    <p>Teste success</p>
+    <div class="containerButton" @click="next">
+        <MyButton text="Return" classButton="Button ButtonNext" />
+    </div>
   </div>
 </template>
 
 <script>
+import MyButton from '../../Micro/MyButton/MyButton.vue'
 export default {
   // eslint-disable-next-line
-    name: 'MySuccess'
+    name: 'MySuccess',
+  components: {
+    MyButton
+  },
+  methods: {
+    next () {
+      this.$store.state.tab = 'basic'
+    }
+  }
 }
 </script>
 
